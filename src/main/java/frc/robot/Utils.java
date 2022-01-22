95fd65d98406a410fffc96e457c5f06314937597
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-01-22 15:35:27 -0800

    feat: utils

---
+++ src/main/java/frc/robot/Utils.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Class for holding utility methods that do not apply to any specific command
+ * or subsystem.
+ */
+public class Utils {
+
+	/**
+	 * If the input is close enough to zero then treat it as zero.
+	 * 
+	 * @param input    The input to apply a dead zone to.
+	 * @param deadZone The absolute range to apply the deadzone.
+	 * @return The dead zoned value.
+	 */
+	public static double deadZones(double input, double deadZone) {
+		if (Math.abs(input) < deadZone) {
+			return 0;
+		}
+		return input;
+	}
+
+	/**
+	 * If "value" is within "tolerance" of "desiredVal," it returns desiredVal.
+	 * </br>
+	 * If "value" is <i>outside</i> the tolerance, then it returns "value."
+	 */
+	public static double tolerance(double value, double desiredVal, double tolerance) {
+		if (Math.abs(value - desiredVal) < tolerance) {
+			return desiredVal;
+		}
+		return value;
+	}
+
+	/**
+	 * Makes lower inputs smaller which allows for finer joystick control.
+	 * 
+	 * @param input The number to apply odd square to.
+	 * @return The odd squared number.
+	 */
+	public static double oddSquare(double input) {
+		return input * Math.abs(input);
+	}
+
+	/**
+	 * Returns an angle between 0 and max if the angle exceeds normal bounds
+	 * 
+	 * @param angle The angle to use.
+	 * @param max   The maximum possible value.
+	 * @return The angle with range of 0 to max.
+	 */
+	public static double normalizeAngle(double angle, double max) {
+		return ((angle % max) + max) % max;
+	}
+}
---
+++ src/test/java/UtilityTest.java
@@ -0,0 +1,49 @@
+import static org.junit.Assert.assertEquals;
+import frc.robot.Utils;
+
+import org.junit.*;
+
+public class UtilityTest {
+    public static final double DELTA = 1e-2; // acceptable deviation range
+    @Test
+    public void deadZonesTest() {
+        double deadZonedResultOne = Utils.deadZones(10, 15);
+        double deadZonedExpectedOne = 0;
+        assertEquals(deadZonedExpectedOne, deadZonedResultOne, DELTA);
+        double deadZonedResultTwo = Utils.deadZones(10, 7);
+        double deadZonedExpectedTwo = 10;
+        assertEquals(deadZonedExpectedTwo, deadZonedResultTwo, DELTA);
+    }
+
+    @Test
+    public void toleranceTest() {
+        double toleranceResultOne = Utils.tolerance(0.5, 1, 1);
+        double toleranceExpectedOne = 1;
+        assertEquals(toleranceExpectedOne, toleranceResultOne, DELTA);
+        double toleranceResultTwo = Utils.tolerance(15, 10, 0.5);
+        double toleranceExpectedTwo = 15;
+        assertEquals(toleranceExpectedTwo, toleranceResultTwo, DELTA);
+
+    }
+
+    @Test
+    public void oddSquareTest() {
+        double oddSquareResultOne = Utils.oddSquare(4);
+        double oddSquareExpectedOne = 16;
+        assertEquals(oddSquareExpectedOne, oddSquareResultOne, DELTA);
+        double oddSquareResultTwo = Utils.oddSquare(-4);
+        double oddSquareExpectedTwo = -16;
+        assertEquals(oddSquareExpectedTwo, oddSquareResultTwo, DELTA);
+    }
+
+    @Test 
+    public void normalizeAngleTest() {
+       double normalizeAngleResultOne = Utils.normalizeAngle(120, 360);
+       double normalizeAngleExpectedOne  = 120;
+       assertEquals(normalizeAngleResultOne, normalizeAngleExpectedOne, DELTA);
+       double normalizeAngleResultTwo = Utils.normalizeAngle(400, 360);
+       double normalizeAngleExpectedTwo = 40;
+       assertEquals(normalizeAngleResultTwo, normalizeAngleExpectedTwo, DELTA); 
+    }
+
+}
