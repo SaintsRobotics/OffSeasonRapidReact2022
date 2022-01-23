e00018262c1a7fdccd0c7027c7fd65fe9a55cf92
LennyAbbas <lennyabbas3@gmail.com>
2022-01-22 18:03:59 -0800

    style: changed spacing

--- src/main/java/frc/robot/Utils.java
+++ src/main/java/frc/robot/Utils.java
@@ -13,53 +13,53 @@ package frc.robot;
  */
 public class Utils {
 
-	/**
-	 * If the input is close enough to zero then treat it as zero.
-	 * 
-	 * @param input    The input to apply a dead zone to.
-	 * @param deadZone The absolute range to apply the deadzone.
-	 * @return The dead zoned value.
-	 */
-	public static double deadZones(double input, double deadZone) {
-		if (Math.abs(input) < deadZone) {
-			return 0;
-		}
-		return input;
-	}
+  /**
+   * If the input is close enough to zero then treat it as zero.
+   * 
+   * @param input    The input to apply a dead zone to.
+   * @param deadZone The absolute range to apply the deadzone.
+   * @return The dead zoned value.
+   */
+  public static double deadZones(double input, double deadZone) {
+    if (Math.abs(input) < deadZone) {
+      return 0;
+    }
+    return input;
+  }
 
-	/**
-	 * If input is within tolerance of desiredVal, it returns desiredVal. If
-	 * input is outside the tolerance, then it returns input.
-	 * 
-	 * @param input      The input number.
-	 * @param desiredVal The desired value.
-	 * @param tolerance  The tolerance.
-	 */
-	public static double tolerance(double input, double desiredVal, double tolerance) {
-		if (Math.abs(input - desiredVal) < tolerance) {
-			return desiredVal;
-		}
-		return input;
-	}
+  /**
+   * If input is within tolerance of desiredVal, it returns desiredVal. If
+   * input is outside the tolerance, then it returns input.
+   * 
+   * @param input      The input number.
+   * @param desiredVal The desired value.
+   * @param tolerance  The tolerance.
+   */
+  public static double tolerance(double input, double desiredVal, double tolerance) {
+    if (Math.abs(input - desiredVal) < tolerance) {
+      return desiredVal;
+    }
+    return input;
+  }
 
-	/**
-	 * Makes lower inputs smaller which allows for finer joystick control.
-	 * 
-	 * @param input The number to apply odd square to.
-	 * @return The odd squared number.
-	 */
-	public static double oddSquare(double input) {
-		return input * Math.abs(input);
-	}
+  /**
+   * Makes lower inputs smaller which allows for finer joystick control.
+   * 
+   * @param input The number to apply odd square to.
+   * @return The odd squared number.
+   */
+  public static double oddSquare(double input) {
+    return input * Math.abs(input);
+  }
 
-	/**
-	 * Returns an angle between 0 and max if the angle exceeds normal bounds
-	 * 
-	 * @param angle The angle to use.
-	 * @param max   The maximum possible value.
-	 * @return The angle with range of 0 to max.
-	 */
-	public static double normalizeAngle(double angle, double max) {
-		return ((angle % max) + max) % max;
-	}
+  /**
+   * Returns an angle between 0 and max if the angle exceeds normal bounds
+   * 
+   * @param angle The angle to use.
+   * @param max   The maximum possible value.
+   * @return The angle with range of 0 to max.
+   */
+  public static double normalizeAngle(double angle, double max) {
+    return ((angle % max) + max) % max;
+  }
 }
