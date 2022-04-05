1dd3053d9fa0155f3c56d0dd98fb825910db40b1
Programming SR <85456157+ProgrammingSR@users.noreply.github.com>
2022-04-04 18:13:28 -0700

    Aaryan fix: increased deadzone constants (#171)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -148,7 +148,7 @@ public final class Constants {
 	public static final class OIConstants {
 		public static final int kDriverControllerPort = 0;
 		public static final int kOperatorControllerPort = 1;
-		public static final double kControllerDeadband = 0.11;
+		public static final double kControllerDeadband = 0.2;
 
 		public static final boolean kTelemetry = false;
 	}
